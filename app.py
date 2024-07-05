74ec7ed440ae0ebe38b255a8a215069565261482
bold <bold.enh@gmail.com>
2024-07-05 15:17:04 +0800

    sudoku

--- app.py
+++ app.py
@@ -1,1 +1,69 @@
-print('heel')
+# File path: sudoku_solver.py
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+
+def find_empty_location(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    # Check row
+    for j in range(len(board[0])):
+        if board[pos[0]][j] == num and pos[1] != j:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check 3x3 grid
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    empty_pos = find_empty_location(board)
+    if not empty_pos:
+        return True
+    else:
+        row, col = empty_pos
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+if __name__ == "__main__":
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    if solve_sudoku(board):
+        print_board(board)
+    else:
+        print("No solution exists")
